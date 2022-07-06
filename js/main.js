f2eae1a0eea7a2c9962feb2049957c02462185d7
Carlo1891 <carlo.18@live.it>
2022-07-06 16:07:13 +0200

    Inizio

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un elemento h1, utilizzando i data nell'istanza di Vue.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa dai data.
